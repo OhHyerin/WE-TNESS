229fa77da79f3e31f8d49ee1c20d657dd283723f
오혜린 <gpfls0506@gmail.com>
2022-07-21 01:08:47 +0900

    refactor : 회원가입 response form 적용

--- backend/src/main/java/backend/controller/UserController.java
+++ backend/src/main/java/backend/controller/UserController.java
@@ -1,8 +1,11 @@
 package backend.controller;
 
 import backend.model.User;
+import backend.model.response.BaseResponseEntity;
 import backend.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +19,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public void registerUser(@RequestBody User user){
+    @ApiOperation(value = "회원가입")
+    public ResponseEntity<BaseResponseEntity> registerUser(@RequestBody User user){
         userService.registerUser(user);
+        return ResponseEntity.status(200).body(new BaseResponseEntity(200, "Success"));
     }
 }
