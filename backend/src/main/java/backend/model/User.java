21602bfa22bcb039d099e2b792829521039e1582
오혜린 <gpfls0506@gmail.com>
2022-07-20 14:11:03 +0900

    feat : properties db연결 추가

---
+++ backend/src/main/java/backend/model/User.java
@@ -0,0 +1,15 @@
+package backend.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    String email;
+    String password;
+    String nickname;
+    String siCode;
+    String gunCode;
+
+
+}
