1b1a46a35ae2a20b720743d561a5664cb01c7de4
김윤석 <rdt2359@gmail.com>
2022-07-20 20:42:29 +0900

    feat : User 회원가입 기능 추가

--- backend/src/main/java/backend/controller/UserController.java
+++ backend/src/main/java/backend/controller/UserController.java
@@ -1,4 +1,22 @@
 package backend.controller;
 
+import backend.model.User;
+import backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public void registerUser(@RequestBody User user){
+        userService.registerUser(user);
+    }
 }
