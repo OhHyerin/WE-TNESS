0812d303799f723492cbd80002fdc3e7f6b3e67a
SSAFY <rdt2359@gmail.com>
2022-07-20 15:28:09 +0900

    feat : User dto 추가

--- backend/src/main/java/backend/model/User.java
+++ backend/src/main/java/backend/model/User.java
@@ -2,15 +2,29 @@ package backend.model;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
 @Data
 public class User {
 
-    String email;
-    String password;
-    String nickname;
-    String siCode;
-    String gunCode;
+    @Id @GeneratedValue
+    private Long id;
+    private String email;
+    private String password;
+    private String nickname;
 
+    private String siCode;
+    private String gunCode;
 
+    private String gender;
+    private double height;
+    private double weight;
+    private String social;
+    private String role;
+    private String socialToken;
+    private boolean banState;
+    private Date banDate;
 
 }
