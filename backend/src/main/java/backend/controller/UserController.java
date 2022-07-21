81c9cdf5695769708f5536f3333ad0f8b08fa224
김윤석 <rdt2359@gmail.com>
2022-07-21 16:52:18 +0900

    feat : User 회원가입 기능 추가 완료

--- backend/src/main/java/backend/controller/UserController.java
+++ backend/src/main/java/backend/controller/UserController.java
@@ -1,13 +1,18 @@
 package backend.controller;
 
 import backend.model.User;
+import backend.model.request.JoinUserDto;
 import backend.model.response.BaseResponseEntity;
 import backend.model.response.DuplicateCheckResDto;
 import backend.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -18,14 +23,20 @@ public class UserController {
 
     @PostMapping("/join")
     @ApiOperation(value = "회원가입")
-    public ResponseEntity<BaseResponseEntity> registerUser(@RequestBody User user){
-        userService.registerUser(user);
-        return ResponseEntity.ok().body(new BaseResponseEntity(200, "Success"));
+    public ResponseEntity<BaseResponseEntity> registerUser(@RequestBody JoinUserDto joinUserDto) {
+        if (joinUserDto.getPassword().equals(joinUserDto.getPwdVerify()) &&
+                userService.checkEmailDuplicate(joinUserDto.getEmail()) &&
+                userService.checkNicknameDuplicate(joinUserDto.getNickname())) {
+            userService.registerUser(joinUserDto.generateUser());
+            return ResponseEntity.ok().body(new BaseResponseEntity(200, "Success"));
+        }
+        return ResponseEntity.internalServerError().body(new BaseResponseEntity(500, "Fail"));
     }
+
 
     @GetMapping("/emailcheck/{email}")
     @ApiOperation(value = "이메일 중복확인")
-    public ResponseEntity<DuplicateCheckResDto> duplicatedEmail (@PathVariable String email){
+    public ResponseEntity<DuplicateCheckResDto> duplicatedEmail(@PathVariable String email) {
         boolean possible = userService.checkEmailDuplicate(email);
 
         //true면 사용가능, false면 이미 존재
@@ -34,7 +45,7 @@ public class UserController {
 
     @GetMapping("/nicknamecheck/{nickname}")
     @ApiOperation(value = "닉네임 중복확인")
-    public ResponseEntity<DuplicateCheckResDto> duplicatedNickname (@PathVariable String nickname){
+    public ResponseEntity<DuplicateCheckResDto> duplicatedNickname(@PathVariable String nickname) {
         boolean possible = userService.checkNicknameDuplicate(nickname);
 
         //true면 사용 가능, false면 이미 존재
