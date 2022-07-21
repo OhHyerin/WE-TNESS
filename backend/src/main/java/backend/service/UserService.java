146bc8ba0f73cb33a086d70ceffb06b989a15ca3
김윤석 <rdt2359@gmail.com>
2022-07-21 09:56:09 +0900

    feat : UserService 인터페이스, 클래스 구조로 변경

--- backend/src/main/java/backend/service/UserService.java
+++ backend/src/main/java/backend/service/UserService.java
@@ -1,18 +1,8 @@
 package backend.service;
 
 import backend.model.User;
-import backend.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
-    @Transactional
-    public void registerUser(User user) {
-        userRepository.save(user);
-    }
+     void registerUser(User user);
 }
---
+++ backend/src/main/java/backend/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package backend.service;
+
+import backend.model.User;
+import backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    @Transactional
+    public void registerUser(User user) {
+        userRepository.save(user);
+    }
+}
