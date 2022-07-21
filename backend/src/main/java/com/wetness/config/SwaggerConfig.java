e76f6acb7a286d4ac7e6b653f0f68e5c3c88e42d
류현수 <neillie23@gmail.com>
2022-07-21 17:26:21 +0900

    refactor : SwaggerConfig 수정

--- backend/src/main/java/com/wetness/config/SwaggerConfig.java
+++ backend/src/main/java/com/wetness/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.
-                        basePackage("com/wetness"))
+                        basePackage("com.wetness"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
