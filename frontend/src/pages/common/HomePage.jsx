966d9f39878333c6f7414465cb5064a530dc548b
JunSeong <gentlmanjun@naver.com>
2022-07-21 11:00:16 +0900

    feat : 메인페이지 구조 생성

--- frontend/src/pages/common/HomePage.jsx
+++ frontend/src/pages/common/HomePage.jsx
@@ -1,10 +1,16 @@
 import { useNavigate } from 'react-router-dom';
+import Banner from '../../components/home/Banner';
+import RankingPreview from '../../components/home/RankingPreview';
+import RoomList from '../../components/home/RoomList';
 
 export default function Home() {
   const navigate = useNavigate();
   return (
     <div>
       <div>
+        <Banner />
+        <RankingPreview />
+        <RoomList />
         <button
           onClick={() => {
             navigate('room/3');
