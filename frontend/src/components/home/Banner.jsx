5248dac66ebc1a1c39f9f1f2699ebc1a9992bc66
JunSeong <gentlmanjun@naver.com>
2022-07-26 17:15:01 +0900

    feat : 테스트용 배너 추가

--- frontend/src/components/home/Banner.jsx
+++ frontend/src/components/home/Banner.jsx
@@ -1,8 +1,36 @@
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Button } from '@mui/material';
+
 export default function Banner() {
+  const items = [
+    {
+      name: 'Random Name #1',
+      description: 'Probably the most random thing you have ever seen!',
+    },
+    {
+      name: 'Random Name #2',
+      description: 'Hello World!',
+    },
+  ];
+
   return (
     <div>
       <h2>배너</h2>
-      {/* mui에는 캐러셀이 없다... https://learus.github.io/react-material-ui-carousel/ 이거 쓰는거 어떰 */}
+      <Carousel>
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
     </div>
+  );
+}
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
   );
 }
