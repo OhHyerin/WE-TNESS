bbf90ad1d35aad4ac18b85b325e96339f0dcce44
JunSeong <gentlmanjun@naver.com>
2022-08-01 21:54:45 +0900

    feat : 네비게이션바 색상 설정 및 배치 수정

--- frontend/src/components/common/NavBar.jsx
+++ frontend/src/components/common/NavBar.jsx
@@ -4,13 +4,27 @@ import styled from 'styled-components';
 import SearchForm from '../search/SearchForm';
 import AccountMenu from './Dropdown';
 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  background: var(--prim-bg-color);
+`;
 const Nav = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 45px;
-  padding: 2px;
-  gap: 10px;
-  background: var(--primary-color);
+  align-items: center;
+  height: 40px;
+  background: var(--sec-bg-color);
+`;
+const NavBtns = styled.div`
+  padding-left: 20px;
+  a {
+    text-decoration: none;
+    font-size: 20px;
+    padding: 0px 10px;
+  }
 `;
 
 const LoginMenu = styled.div`
@@ -21,21 +35,31 @@ export default function NavBar() {
   const isAuthenticated = useSelector(state => state.user.isAuthenticated);
 
   return (
-    <Nav>
-      <Link to="/">Home</Link>
-      <SearchForm></SearchForm>
-      <div>
-        {isAuthenticated ? (
-          <LoginMenu>
-            <AccountMenu />
-          </LoginMenu>
-        ) : (
-          <LoginMenu>
-            <Link to="/signup">회원가입</Link>
-            <Link to="/login">로그인</Link>
-          </LoginMenu>
-        )}
-      </div>
-    </Nav>
+    <>
+      <Header>
+        <Link to="/">Logo</Link>
+        <SearchForm></SearchForm>
+        <div>
+          {isAuthenticated ? (
+            <LoginMenu>
+              <AccountMenu />
+            </LoginMenu>
+          ) : (
+            <LoginMenu>
+              <Link to="/signup">회원가입</Link>
+              <Link to="/login">로그인</Link>
+            </LoginMenu>
+          )}
+        </div>
+      </Header>
+      <Nav>
+        <NavBtns>
+          <Link to="/">홈</Link>
+          <Link to="history/:userNickname">기록</Link>
+          <Link to="ranking">랭킹</Link>
+        </NavBtns>
+        <div></div>
+      </Nav>
+    </>
   );
 }
--- frontend/src/components/search/SearchForm.jsx
+++ frontend/src/components/search/SearchForm.jsx
@@ -1,11 +1,32 @@
+import { Search } from '@mui/icons-material';
 import styled from 'styled-components';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
 
-const InputForm = styled.div``;
+const SearchIconWrapper = styled.div`
+  padding: 0, 2;
+  height: '100%';
+  position: 'absolute';
+  pointer-events: 'none';
+  display: 'flex';
+  align-items: 'center';
+  justify-content: 'center';
+`;
+
+const StyledInputBase = styled.InputBase`
+  color: 'inherit';
+  padding: 1, 1, 1, 0;
+  padding-left: ${`calc(1em + 4)`};
+  width: '100%';
+`;
 
 export default function SearchForm() {
   return (
-    <InputForm>
-      <input type="text" />
-    </InputForm>
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+    </Search>
   );
 }
