331e7bf068b8f9bceda7460f582986450e51930e
DK <ehdrms121@ajou.ac.kr>
2022-08-01 18:01:02 +0900

    style: 닉네임&이메일 체크박스 위치 조정 / 카카오 로그인 버튼 포인터 수정

--- frontend/src/components/common/CheckBtn.jsx
+++ frontend/src/components/common/CheckBtn.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
-const CheckBtn = styled.button`
+const CheckBtn = styled.div`
   display: flex;
+  height: 55.98px;
   justify-content: center;
   align-items: center;
-  padding: 5px 10px;
   color: white;
+  margin-right: -14px;
+  padding: 0px 5px;
   background-color: ${props => props.deactive? "gray" : "#233142"};
   border-radius: 5px;
   :hover {
