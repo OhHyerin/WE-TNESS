f8beebb9b2d867ac7d7421fc8addb9f6e4364b62
JunSeong <gentlmanjun@naver.com>
2022-08-02 14:22:03 +0900

    refactor : 배너 이미지 배열 구조 변경

--- frontend/src/components/home/Banner.jsx
+++ frontend/src/components/home/Banner.jsx
@@ -1,25 +1,13 @@
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
-import tutorial from '../../assets/images/banner/tutorial.png';
-import ranking from '../../assets/images/banner/ranking.png';
+import items from '../../assets/data/bannerItems';
 
 const bannerHeight = '450px';
 
 export default function Banner() {
-  const items = [
-    {
-      name: 'Random Name #1',
-      img: tutorial,
-    },
-    {
-      name: 'Random Name #2',
-      img: ranking,
-    },
-  ];
-
   return (
     <>
-      <h2>배너</h2>
+      {/* 배너 */}
       <Carousel height={bannerHeight} interval={'6000'} navButtonsAlwaysVisible={'true'}>
         {items.map((item, i) => (
           <Item key={i} item={item} />
@@ -32,8 +20,6 @@ function Item(props) {
   return (
     <Paper>
       <img src={props.item.img} alt="tutorial img" height={bannerHeight}></img>
-
-      {/* <Button className="CheckButton">Check it out!</Button> */}
     </Paper>
   );
 }
