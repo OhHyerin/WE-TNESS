1b6f7b7fae6d18fed951fbbd908d270131e48c36
JunSeong <gentlmanjun@naver.com>
2022-08-04 16:17:55 +0900

    feat : 컬러 팔레트 설정 및 전역 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
 import { getAccessToken, getCurrentUser } from './features/Token';
 import { fetchCurrentUser, checkLogin, toggleIsLoding } from './features/user/UserSlice';
 import NavBar from './components/common/NavBar';
 import GlobalStyle from './styles/GlobalStyle';
 import Resolution from './styles/Resolution';
+import theme from './styles/Theme';
 
 function App() {
   const dispatch = useDispatch();
@@ -23,13 +25,15 @@ function App() {
     return (
       <div>
         <GlobalStyle />
-        <NavBar></NavBar>
+        <ThemeProvider theme={theme}>
+          <NavBar></NavBar>
 
-        <Resolution>
-          <div>
-            <Outlet></Outlet>
-          </div>
-        </Resolution>
+          <Resolution>
+            <div>
+              <Outlet></Outlet>
+            </div>
+          </Resolution>
+        </ThemeProvider>
       </div>
     );
   }
