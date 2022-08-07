0c5cef56da226956068a82619e302b3bdad5ecb8
한유연 <hyy0416@naver.com>
2022-08-08 00:29:19 +0900

    feat: aws s3관련 세팅, 업로드 util 추가

--- backend/src/main/java/com/wetness/BackendApplication.java
+++ backend/src/main/java/com/wetness/BackendApplication.java
@@ -2,12 +2,19 @@ package com.wetness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BackendApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+			"classpath:application.properties, " +
+			"classpath:application-aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		new SpringApplicationBuilder(BackendApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
---
+++ backend/src/main/java/com/wetness/util/AwsS3Util.java
@@ -0,0 +1,81 @@
+package com.wetness.util;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AwsS3Util {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        File uploadFile = convert(multipartFile)  // 파일 변환할 수 없으면 에러
+                .orElseThrow(() -> new IllegalArgumentException("error: MultipartFile -> File convert fail"));
+
+        return upload(uploadFile, dirName);
+    }
+
+    // S3로 파일 업로드하기
+    private String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();   // S3에 저장된 파일 이름
+        String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    // S3로 업로드
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    // 로컬에 저장된 이미지 지우기
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("File delete success");
+            return;
+        }
+        log.info("File delete fail");
+    }
+
+    // 로컬에 파일 업로드 하기
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        System.out.println(System.getProperty("user.dir")+" ========================user.dir");
+        System.out.println(file.getOriginalFilename()+" file Name ===============");
+
+        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
+
+        System.out.println("오류 어디까지 왔을까? ========================1==============");
+
+        if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
+
+            System.out.println("오류 어디까지 왔을까? ========================2=============");
+
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
+                System.out.println("오류 어디까지 왔을까? ========================3=============");
+                fos.write(file.getBytes());
+
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
+}
