9bcc945c8c896f3eb785aadbff0ba75aaa6ce9c8
JunSeong <gentlmanjun@naver.com>
2022-08-10 02:36:35 +0900

    feat : 팔로우, 팔로잉 목록 이동

--- frontend/src/components/myPage/UserProfile.jsx
+++ frontend/src/components/myPage/UserProfile.jsx
@@ -1,6 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Button, Modal } from '@mui/material';
+import styled from 'styled-components';
 import { fetchFollowerList, fetchFollowingList } from '../../features/user/UserSlice';
+import FollowerList from './FollowerList';
+import FollowingList from './FollowingList';
+
+const FollowBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
+  width: 50%;
+  justify-content: space-evenly;
+`;
 
 export default function UserProfile(props) {
   const dispatch = useDispatch();
@@ -13,12 +25,29 @@ export default function UserProfile(props) {
   const followerList = useSelector(state => state.user.followerList);
   const followingList = useSelector(state => state.user.followingList);
 
+  const [openFollower, setOpenFollower] = useState(false);
+  const [openFollowing, setOpenFollowing] = useState(false);
+
+  // 팔로워 핸들러
+  const handleOpenFollower = () => {
+    setOpenFollower(true);
+    console.log(followerList);
+  };
+  const handleCloseFollower = () => setOpenFollower(false);
+
+  // 팔로잉 핸들러
+  const handleOpenFollowing = () => {
+    setOpenFollowing(true);
+    console.log(followingList);
+  };
+  const handleCloseFollowing = () => setOpenFollowing(false);
+
   return (
     <>
       <div>유저 네임 : {props.userNickname}</div>
       <div>
-        <span>팔로워 : {followerList.length} </span>
-        <span>팔로잉 : {followingList.length}</span>
+        <Button onClick={handleOpenFollower}>팔로워 :{followerList.length}</Button>
+        <Button onClick={handleOpenFollowing}>팔로잉 :{followingList.length}</Button>
       </div>
       <div>
         <span>총 경기 수 : </span>
@@ -26,6 +55,18 @@ export default function UserProfile(props) {
         <span>2등 : </span>
         <span>3등 : </span>
       </div>
+
+      {/* 모달 - 팔로워, 팔로잉 */}
+      <Modal open={openFollower} onClose={handleCloseFollower}>
+        <FollowBox>
+          <FollowerList followerList={followerList} />
+        </FollowBox>
+      </Modal>
+      <Modal open={openFollowing} onClose={handleCloseFollowing}>
+        <FollowBox>
+          <FollowingList followingList={followingList} />
+        </FollowBox>
+      </Modal>
     </>
   );
 }
