0c1344126aeaeeec1588d49d07480061016dcbf2
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-08-12 02:42:45 +0900

    feat: history page 뱃지 이미지 추가 / 배치 완료

--- frontend/src/assets/data/awardItems.js
+++ frontend/src/assets/data/awardItems.js
@@ -1,22 +1,164 @@
-import award1 from '../images/award/1.svg';
-import award2 from '../images/award/2.svg';
-import award3 from '../images/award/3.svg';
+import award1 from '../images/award/001.svg';
+import award2 from '../images/award/002.svg';
+import award3 from '../images/award/003.svg';
+import award4 from '../images/award/004.svg';
+import award5 from '../images/award/005.svg';
+import award6 from '../images/award/006.svg';
+import award7 from '../images/award/007.svg';
+import award8 from '../images/award/008.svg';
+import award9 from '../images/award/009.svg';
+import award10 from '../images/award/010.svg';
+import award11 from '../images/award/011.svg';
+import award12 from '../images/award/012.svg';
+import award13 from '../images/award/013.svg';
+import award14 from '../images/award/014.svg';
+import award15 from '../images/award/015.svg';
+import award16 from '../images/award/016.svg';
+import award17 from '../images/award/017.svg';
+import award18 from '../images/award/018.svg';
+import award19 from '../images/award/019.svg';
+import award20 from '../images/award/020.svg';
+import award21 from '../images/award/021.svg';
+import award22 from '../images/award/022.svg';
+import award23 from '../images/award/023.svg';
+import award24 from '../images/award/024.svg';
+import award25 from '../images/award/025.svg';
+import award26 from '../images/award/026.svg';
+import award27 from '../images/award/027.svg';
+import award28 from '../images/award/028.svg';
+import award29 from '../images/award/029.svg';
+import award30 from '../images/award/030.svg';
+import award31 from '../images/award/031.svg';
+import award32 from '../images/award/032.svg';
+import award33 from '../images/award/033.svg';
+import award34 from '../images/award/034.svg';
+import award35 from '../images/award/035.svg';
+import award36 from '../images/award/036.svg';
+import award37 from '../images/award/037.svg';
+import award38 from '../images/award/038.svg';
+import award39 from '../images/award/039.svg';
+import award40 from '../images/award/040.svg';
 
 const awards = [
   {
     id: 1,
-    img: award1,
+    notAcheiveImg: award1,
+    acheiveImg: award2,
     description: '첫 번째 메달',
   },
   {
     id: 2,
-    img: award2,
-    description: '두 번째 메달',
+    notAcheiveImg: award3,
+    acheiveImg: award4,
+    description: '첫 번째 메달',
   },
   {
     id: 3,
-    img: award3,
-    description: '세 번째 메달',
+    notAcheiveImg: award5,
+    acheiveImg: award6,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 4,
+    notAcheiveImg: award7,
+    acheiveImg: award8,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 5,
+    notAcheiveImg: award9,
+    acheiveImg: award10,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 6,
+    notAcheiveImg: award11,
+    acheiveImg: award12,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 7,
+    notAcheiveImg: award13,
+    acheiveImg: award14,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 8,
+    notAcheiveImg: award15,
+    acheiveImg: award16,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 9,
+    notAcheiveImg: award17,
+    acheiveImg: award18,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 10,
+    notAcheiveImg: award19,
+    acheiveImg: award20,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 11,
+    notAcheiveImg: award21,
+    acheiveImg: award22,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 12,
+    notAcheiveImg: award23,
+    acheiveImg: award24,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 13,
+    notAcheiveImg: award25,
+    acheiveImg: award26,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 14,
+    notAcheiveImg: award27,
+    acheiveImg: award28,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 15,
+    notAcheiveImg: award29,
+    acheiveImg: award30,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 16,
+    notAcheiveImg: award31,
+    acheiveImg: award32,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 17,
+    notAcheiveImg: award33,
+    acheiveImg: award34,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 18,
+    notAcheiveImg: award35,
+    acheiveImg: award36,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 19,
+    notAcheiveImg: award37,
+    acheiveImg: award38,
+    description: '첫 번째 메달',
+  },
+  {
+    id: 20,
+    notAcheiveImg: award39,
+    acheiveImg: award40,
+    description: '첫 번째 메달',
   },
 ];
 
