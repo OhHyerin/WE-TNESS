79f5320891cd7a3541b78ebd16335de8b558df0e
류현수 <neillie23@gmail.com>
2022-08-12 17:56:30 +0900

    Update WebSecurityConfig.java

--- backend/src/main/java/com/wetness/config/WebSecurityConfig.java
+++ backend/src/main/java/com/wetness/config/WebSecurityConfig.java
@@ -45,8 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
     private static final String[] Exclude_Paths =
-            {"api/", "api/user/login/**", "api/user/join","api/user/duplicate-email/*", "api/user/duplicate-nickname/*",
-             "api/user/refresh", "/room/disconnect", "/swagger-ui.html", "/webjars/springfox-swagger-ui/**"
+            {"/api/", "/api/user/login/**", "/api/user/join","/api/user/duplicate-email/*", "/api/user/duplicate-nickname/*",
+             "/api/user/refresh", "/room/disconnect", "/swagger-ui.html", "/webjars/springfox-swagger-ui/**"
                     , "/swagger-resources/**","/v2/api-docs","/csrf", "/error", "/report", "/report/*", "/user/send-pw"};
 //"/user/duplicate-email/*",
     @Override
