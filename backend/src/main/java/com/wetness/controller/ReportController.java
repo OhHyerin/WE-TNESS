d2e407519398ee083969abc96abde36110df990c
한유연 <hyy0416@naver.com>
2022-08-14 01:38:00 +0900

    refactor: controller 경로 /api 붙임

--- backend/src/main/java/com/wetness/controller/ReportController.java
+++ backend/src/main/java/com/wetness/controller/ReportController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/report")
+@RequestMapping("/api/report")
 @RequiredArgsConstructor
 public class ReportController {
 
