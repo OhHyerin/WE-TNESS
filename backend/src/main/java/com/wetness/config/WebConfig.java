8ecb5e8d1c21378b7b185f9715fb6f7fe01791cd
한유연 <hyy0416@naver.com>
2022-08-15 01:58:02 +0900

    test: annotation

--- backend/src/main/java/com/wetness/config/WebConfig.java
+++ backend/src/main/java/com/wetness/config/WebConfig.java
@@ -3,7 +3,10 @@ package com.wetness.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
