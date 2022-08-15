3ed08d3ef89beb4441f16e95ecf92881e63add6f
한유연 <hyy0416@naver.com>
2022-08-15 19:19:22 +0900

    test: proxy 설정 해제해봄

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createPoxyMiddleware, createProxyMiddleware } = require('http-proxy-middleware');
+// const { createPoxyMiddleware, createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://i7a205.p.ssafy.io:5000',
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'http://i7a205.p.ssafy.io:5000',
+//       changeOrigin: true,
+//     })
+//   );
+// };
