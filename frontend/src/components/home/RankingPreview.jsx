078f41da6ddba266fc621747b6a284582e00935e
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-08-16 23:14:40 +0900

    feat: 실시간 랭킹 정보 패치

--- frontend/src/components/home/RankingPreview.jsx
+++ frontend/src/components/home/RankingPreview.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import {
+  fetchTotalRanking,
+  fetchSquatRanking,
+  fetchPushupRanking,
+  fetchBurpeeRanking,
+  fetchLungeRanking,
+} from '../../features/rank/RankSlice';
 import './RankingPreview.css';
 
 const PriviewBanner = styled.div`
@@ -6,27 +15,73 @@ const PriviewBanner = styled.div`
 `;
 
 export default function RankingPreview() {
+  const dispatch = useDispatch();
+
+  const rank = useSelector(state => state.rank.previewRank);
+
+  useEffect(() => {
+    dispatch(fetchTotalRanking({ workoutId: [1, 2, 3, 4], selectGugun: false }));
+    dispatch(fetchSquatRanking({ workoutId: [1], selectGugun: false }));
+    dispatch(fetchPushupRanking({ workoutId: [2], selectGugun: false }));
+    dispatch(fetchBurpeeRanking({ workoutId: [3], selectGugun: false }));
+    dispatch(fetchLungeRanking({ workoutId: [4], selectGugun: false }));
+  }, [dispatch]);
+
   return (
     <PriviewBanner>
-      <div class="center">
-        <div class="carousel">
-          <div class="pre">실시간 랭킹</div>
-          <div class="change_outer">
-            <div class="change_inner">
-              <div class="element">
-                <p>전체</p>
+      <div className="center">
+        <div className="carousel">
+          <div className="pre">실시간 랭킹</div>
+          <div className="change_outer">
+            <div className="change_inner">
+              <div className="element">
+                {rank?.total ? (
+                  <p>
+                    전체 1등 {rank?.total[0]?.userNickname} 2등 {rank.total[1]?.userNickname} 3등{' '}
+                    {rank.total[2]?.userNickname}
+                  </p>
+                ) : (
+                  '랭킹없음'
+                )}
               </div>
-              <div class="element">
-                <p>스쿼트</p>
+              <div className="element">
+                {rank?.squat ? (
+                  <p>
+                    스쿼트1등 {rank?.squat[0]?.userNickname} 2등 {rank.squat[1]?.userNickname} 3등{' '}
+                    {rank.squat[2]?.userNickname}
+                  </p>
+                ) : (
+                  '랭킹없음'
+                )}
               </div>
-              <div class="element">
-                <p>팔굽혀펴기</p>
+              <div className="element">
+                {rank?.pushup ? (
+                  <p>
+                    팔굽혀펴기 1등 {rank.pushup[0]?.userNickname} 2등 {rank.pushup[1]?.userNickname} 3등{' '}
+                  </p>
+                ) : (
+                  '랭킹없음'
+                )}
               </div>
-              <div class="element">
-                <p>버피</p>
+              <div className="element">
+                {rank?.burpee ? (
+                  <p>
+                    버피 1등 {rank.burpee[0]?.userNickname} 2등 {rank.burpee[1]?.userNickname} 3등{' '}
+                    {rank.burpee[2]?.userNickname}
+                  </p>
+                ) : (
+                  '랭킹없음'
+                )}
               </div>
-              <div class="element">
-                <p>런지</p>
+              <div className="element">
+                {rank?.lunge ? (
+                  <p>
+                    런지 1등 {rank.lunge[0]?.userNickname} 2등 {rank.lunge[1]?.userNickname} 3등{' '}
+                    {rank.lunge[2]?.userNickname}
+                  </p>
+                ) : (
+                  '랭킹없음'
+                )}
               </div>
             </div>
           </div>
