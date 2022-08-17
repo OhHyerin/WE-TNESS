6480f85edb54951630a44f9bcf8faf42435205af
류현수 <neillie23@gmail.com>
2022-08-17 16:42:31 +0900

    Update frontend/src/api/Oauth.js

--- frontend/src/api/Oauth.js
+++ frontend/src/api/Oauth.js
@@ -1,5 +1,5 @@
 const REST_API_KEY = "376a57e7cef85c6560a77351c93a9b1f";
-const REDIRECT_URI =  'https://i7a205.p.ssafy.io/user/login/kakao'
+const REDIRECT_URI =  'https://i7a205.p.ssafy.io/api/user/login/kakao'
 
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
