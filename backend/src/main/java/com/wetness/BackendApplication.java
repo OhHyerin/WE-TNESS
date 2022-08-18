e2fc7ab70b5cd5fdaee0dea1beb9f3dc307886cc
한유연 <hyy0416@naver.com>
2022-08-18 22:33:08 +0900

    test: configuration

--- backend/src/main/java/com/wetness/BackendApplication.java
+++ backend/src/main/java/com/wetness/BackendApplication.java
@@ -7,9 +7,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class BackendApplication {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
-			"classpath:application.properties";
-//			"classpath:application-aws.yml, " +
-//			"classpath:application-mail.yml ";
+			"classpath:application.properties+"+
+			"classpath:application-aws.yml, " +
+			"classpath:application-mail.yml " +
+			"classpath:application-kakao.yml";
 
 
 	public static void main(String[] args) {
