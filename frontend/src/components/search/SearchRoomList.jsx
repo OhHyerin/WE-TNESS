9138b7c7feee30121b7362b19ad7bc45b94d64b3
DK <ehdrms121@ajou.ac.kr>
2022-08-18 20:38:07 +0900

    feat: 닉네임 chip 수정

--- frontend/src/components/search/SearchRoomList.jsx
+++ frontend/src/components/search/SearchRoomList.jsx
@@ -1,8 +1,23 @@
-import { List } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import styled from 'styled-components';
 import RoomCard from '../common/RoomCard';
 import { searchRooms } from '../../features/room/RoomSlice';
+
+const List = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: 50px;
+  gap: 20px;
+`;
+
+const NoRoomBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  font-size: large;
+`;
 
 export default function SearchRoomList() {
   const searchRoomResult = useSelector(state => state.room.searchRoomResult);
@@ -23,14 +38,14 @@ export default function SearchRoomList() {
       <h2>검색된 방 목록</h2>
       {searchRoomResult.length > 0 ? (
         <List>
-          {searchRoomResult.map(room =>
+          {searchRoomResult.map((room, i) =>
             room.locked === showPrivate && (workout === 0 ? true : workout === room.workoutId) ? (
-              <RoomCard room={room} />
+              <RoomCard key={i} room={room} />
             ) : null
           )}
         </List>
       ) : (
-        <div>검색된 방이 없습니다.</div>
+        <NoRoomBox>검색된 방이 없습니다.</NoRoomBox>
       )}
     </div>
   );
