7266512803d1b72dada7cc829006177e81f2e31a
한유연 <hyy0416@naver.com>
2022-08-23 22:24:46 +0900

    test: cors test with gentlman

--- backend/src/main/java/com/wetness/config/WebSecurityConfig.java
+++ backend/src/main/java/com/wetness/config/WebSecurityConfig.java
@@ -68,6 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
+/***
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -83,5 +84,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return source;
     }
 
-
+***/
 }
